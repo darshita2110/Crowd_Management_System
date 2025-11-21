91d9126895028d1eddcd260da124d5f4ea351e00
darshita2110 <darshitabansal211005@gmail.com>
2025-11-21 19:00:02 +0530

    app build done.

--- app_crowd_buddy/lib/utils/constants.dart
+++ app_crowd_buddy/lib/utils/constants.dart
@@ -1,7 +1,7 @@
 class ApiConstants {
   ApiConstants._();
 
-  static const String baseUrl = 'https://wzwc22dw-8000.inc1.devtunnels.ms';
+  static const String baseUrl = 'https://crowd-management-system-lx1h.onrender.com';
 
   // Endpoints
   static const String registerEndpoint = '/auth/register';
